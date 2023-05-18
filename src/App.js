1f987798323572e849298f0a52a8f951441d7795
Arditti93 <alex.arditti@hotmail.com>
2023-05-18 15:40:27 +0100

    app finshed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import {useEffect, useState} from "react"
+import MovieCard from "./componets/MovieCard"
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=7d2e5ef7"
 
 function App() {
+
+  // hold the value in the input box when a user types something in
+  const [searchTerm, setSearchTerm] = useState("")
+  const [movies, setMovies] = useState([])
+
+  useEffect (() => {
+    // searchFilms("James Bond")
+  }, [])
+
+
+  const searchFilms = async (title) => {
+    const req = await fetch(`${API_URL}&s=${title}`)
+    const res = await req.json()
+    console.log(res)
+    setMovies(res.Search)
+  }   
+
   return (
     <div className="App">
       <h1>My amazing movie app</h1>
+
+      <div className="search">
+        <input
+            placeholde="search for a film"
+            onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button onClick={() => searchFilms(searchTerm)}>
+            Click to search for a film
+        </button>
+      </div>
+
+      { 
+        movies?.length > 0
+        // if the movies array is greater than zero i.e movies have been returned from our api
+        ? (
+          <div className='container'>
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+          // if movies array is less than zero i.e no movies have been returned from our api
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
     </div>
   );
 }
